fed21cce65be331b056c74a6f2bc1427b02888e9
tijeffrey <112028948+tijeffrey@users.noreply.github.com>
2023-07-16 23:21:13 -0400

    index

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize');
+
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'path/to/database.sqlite'
+  });
+
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
+
